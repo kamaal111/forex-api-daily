e27bcf86d2ea02646269d8863064d3167b730ade
Kamaal <kamaal.f1@gmail.com>
2023-02-11 21:48:28 +0100

    Storing rates in a file

--- daily.py
+++ daily.py
@@ -1,9 +1,11 @@
+import json
 import requests
 import xmltodict
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Dict, List, TypedDict
+from typing import Any, Dict, List, TypedDict
 from bs4 import BeautifulSoup
+from pathlib import Path
 
 
 BASE_FOREX_URL = "https://www.ecb.europa.eu"
@@ -55,22 +57,50 @@ CURRENCIES = [
 
 def main():
     forex_urls = get_forex_urls()
-    exchange_rates = fetch_exchange_rates(forex_urls)
-    print(f"{exchange_rates=}")
+    exchange_rates = fetch_exchange_rates(urls=forex_urls)
+    store_exchange_rates(exchange_rates=exchange_rates)
 
 
-# def
+def store_exchange_rates(exchange_rates: Dict[datetime, "RecordExchangeRate"]):
+    rates_file = Path("rates.json")
+    rates_file_content = get_rates_file_content(file_content=rates_file.read_text())
+
+    items_to_store: List[Dict[str, Any]] = []
+    for exchange_rate_key, exchange_rate in exchange_rates.items():
+        if (rate_file_item := rates_file_content.get(exchange_rate_key)) and (
+            rate_file_item := rate_file_item.get(exchange_rate.base)
+        ):
+            print("found")
+            items_to_store.append(rate_file_item.to_dict())
+        else:
+            print("adding new")
+            items_to_store.append(exchange_rate.to_dict())
+
+    rates_file.write_text(json.dumps(items_to_store, indent=2))
+
+
+def get_rates_file_content(file_content: str):
+    rates_mapped_by_date: Dict[datetime, Dict[str, RecordExchangeRate]] = {}
+    for content in json.loads(file_content):
+        exchange_rate = RecordExchangeRate.from_dict(data=content)
+        if rates_mapped_by_date.get(exchange_rate.date) is None:
+            rates_mapped_by_date[exchange_rate.date] = {}
+
+        rates_mapped_by_date[exchange_rate.date][exchange_rate.base] = exchange_rate
+
+    return rates_mapped_by_date
 
 
 def fetch_exchange_rates(urls: List[str]):
     exchange_rates: Dict[datetime, RecordExchangeRate] = {}
 
     for url in urls:
+        print(f"getting data from {url=}")
         response = requests.get(url).content
         data = xmltodict.parse(response)
 
         for item in data.get("rdf:RDF", {}).get("item", []):
-            item = ForexItem.from_ecb_response(item)
+            item = ForexItem.from_ecb_response(response=item)
 
             if exchange_rates.get(item.date) is None:
                 exchange_rates[item.date] = RecordExchangeRate(
@@ -102,8 +132,20 @@ class RecordExchangeRate:
     base: str
     rates: Dict[str, float]
 
+    def to_dict(self):
+        return {
+            "date": self.date.strftime("%Y-%m-%dT%H:%M:%S"),
+            "base": self.base,
+            "rates": self.rates,
+        }
+
     def add_rate(self, currency: str, value: float):
         self.rates[currency] = value
+
+    def from_dict(data: Dict[str, Any]) -> "RecordExchangeRate":
+        date = datetime.strptime(data["date"], "%Y-%m-%dT%H:%M:%S")
+
+        return RecordExchangeRate(date=date, base=data["base"], rates=data["rates"])
 
 
 @dataclass
@@ -136,7 +178,7 @@ class ForexItem:
     def from_ecb_response(response: "ForexItemDict") -> "ForexItem":
         date = datetime.strptime(response["dc:date"].split("+")[0], "%Y-%m-%dT%H:%M:%S")
         statistics = ForexItemExchangeRate.from_ecb_response(
-            response["cb:statistics"]["cb:exchangeRate"]
+            response=response["cb:statistics"]["cb:exchangeRate"]
         )
 
         return ForexItem(statistics=statistics, date=date)
